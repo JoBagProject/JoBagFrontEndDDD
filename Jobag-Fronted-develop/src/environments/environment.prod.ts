3ba89a3e7fd9466a8546c0c90ed6c401808c8c2b
Schmidt <56980361+elcrack2001@users.noreply.github.com>
2022-11-13 16:12:25 -0500

    Despliegue en Firebase
    Se desplego el FrontEnd en Firebase

--- Jobag-Fronted-develop/src/environments/environment.prod.ts
+++ Jobag-Fronted-develop/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyArskQaryhcCjxHAZQH3cI4sXS2Pv7VRgU",
+    authDomain: "jobagweb.firebaseapp.com",
+    projectId: "jobagweb",
+    storageBucket: "jobagweb.appspot.com",
+    messagingSenderId: "449937287461",
+    appId: "1:449937287461:web:87b035cf1806fa8548d2cc"
+  }
 };
--- Jobag-Fronted-develop/src/environments/environment.ts
+++ Jobag-Fronted-develop/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyArskQaryhcCjxHAZQH3cI4sXS2Pv7VRgU",
+    authDomain: "jobagweb.firebaseapp.com",
+    projectId: "jobagweb",
+    storageBucket: "jobagweb.appspot.com",
+    messagingSenderId: "449937287461",
+    appId: "1:449937287461:web:87b035cf1806fa8548d2cc"
+  }
 };
 
 /*
